3b9a2464564cd846b90fec1814e16fb8ad4d30a6
Cheejyg <glenncheejunyuan@hotmail.com>
2019-01-19 03:25:29 +0800

    verbosity

--- Genetic Algorithms for Swarm Parameter Tuning.py
+++ Genetic Algorithms for Swarm Parameter Tuning.py
@@ -406,12 +406,12 @@ def __global__() -> None:
 		)
 	
 	# OVERRIDE
+	verbosity = 2
 	dimension = 2
 	n = 400
 	nPredators = 4
 	nPreys = 4
 	ticks = 1000
-	# verbosity = 1
 	if positions.shape[0] != n or positions.shape[1] != dimension \
 		or positionsPredator.shape[0] != nPredators or positionsPredator.shape[1] != dimension \
 		or positionsPrey.shape[0] != nPreys or positionsPrey.shape[1] != nPreys:
